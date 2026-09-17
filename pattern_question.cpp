a8545be54910ec3a144c6fa5f18ed2f131707f6d
Raja <rajaaax7@gmail.com>
2026-09-17 21:38:47 +0530

    Upload

--- pattern_question.cpp
+++ pattern_question.cpp
@@ -37,20 +37,24 @@
 // }
 
 
-
 #include<iostream>
 using namespace std;
-void print2(int n) {
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<=i; j++) {
-            cout<<"*";
+void print1(int n) {
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout<<j << " ";
         }
         cout<<endl;
     }
 }
-
 int main() {
-    int n;
-    cin>>n;
-    print2(n);
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        int n; 
+        cin>> n;
+        print1(n);
+    }
+
+    // return 0;
 }
