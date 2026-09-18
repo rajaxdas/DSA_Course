95687896235f4608d5577cebcffb7709e38ca24b
Raja <rajaaax7@gmail.com>
2026-09-18 22:41:51 +0530

    Upload

--- pattern_question.cpp
+++ pattern_question.cpp
@@ -61,20 +61,89 @@
 
 
 // ------->repeating numbers<--------
+// #include<iostream>
+// using namespace std;
+// void print4(int n) {
+// for(int i=1; i<=n; i++) {
+//     for(int j=1; j<=i; j++) {
+//         cout<< i << " ";
+//     }
+//     cout<< endl;
+//     }
+// }
+// int main() {
+//     int t;
+//     cin>>t;
+//     int n;
+//     cin>>n;
+//     print4(n); 
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// void print5(int n) {
+//     for(int i=1; i<=n; i++) {
+//         for(int j=0; j<n-i+1; j++) {
+//             cout<< "*";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main() {
+//     int t;
+//     cin>>t;
+//     int n;
+//     cin>>n;
+//     print5(t);
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+//     void print6(int n) {
+//         for(int i=1; i<n; i++) {
+//             for(int j=1; j<n-i; j++) {
+//                 cout<< j << "";
+//             }
+//             cout<<endl;
+//         }
+//     }
+// int main() {
+//     int t;
+//     cin>>t;
+//     int n;
+//     cin>>n;
+//     print6(t);
+// }
+
+
 #include<iostream>
 using namespace std;
-void print4(int n) {
-for(int i=1; i<=n; i++) {
-    for(int j=1; j<=i; j++) {
-        cout<< i << " ";
+    void print6(int n) {
+        for(int i=0; i<n; i++) {
+                // space
+
+        for(int j=0; j<n-i-1; j++) {
+                cout<< " ";
+        }
+                // star
+        for(int j=0; j<2*i+1; j++) {
+                cout<<"*";
+        }
+                // space
+        for(int j=0; j<n-i-1; j++) {
+                    cout<<" ";
+        }
+            cout<<endl;
+        }
     }
-    cout<< endl;
-    }
-}
 int main() {
     int t;
     cin>>t;
     int n;
     cin>>n;
-    print4(n); 
+    print6(t);
 }
