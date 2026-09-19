1d57329f267644db950bc927ab271eaf31f9de4f
Raja <rajaaax7@gmail.com>
2026-09-19 18:02:44 +0530

    Upload

--- pattern_question.cpp
+++ pattern_question.cpp
@@ -79,7 +79,7 @@
 //     print4(n); 
 // }
 
-
+//------->print stars<--------
 // #include<iostream>
 // using namespace std;
 // void print5(int n) {
@@ -100,7 +100,7 @@
 
 
 
-
+// --------->print numbers<--------
 // #include<iostream>
 // using namespace std;
 //     void print6(int n) {
@@ -120,30 +120,72 @@
 // }
 
 
+// ------->print stars<--------
+// #include<iostream>
+// using namespace std;
+// void print7(int n) {
+//         for(int i=0; i<n; i++) {
+//                 //space 
+//                 for(int j=0; j<i; j++) {
+//                         cout<<" ";
+//                 }
+//                 //star
+//                 for(int j=0; j<2*n - (2*i + 1); j++) {
+//                         cout<<"*";
+//                 }
+//                 for(int j=0; j<i; j++) {
+//                         cout<<" ";
+//                 }
+//                 cout<<endl;
+//         }
+// }
+
+//     void print6(int n) {
+//         for(int i=0; i<n; i++) {
+//                 // space
+
+//         for(int j=0; j<n-i-1; j++) {
+//                 cout<< " ";
+//         }
+//                 // star
+//         for(int j=0; j<2*i+1; j++) {
+//                 cout<<"*";
+//         }
+//                 // space
+//         for(int j=0; j<n-i-1; j++) {
+//                     cout<<" ";
+//         }
+//             cout<<endl;
+//         }
+//     }
+// int main() {
+//     int t;
+//     cin>>t;
+//     int n;
+//     cin>>n;
+//     print6(n);
+//     print7(n);
+// }
+
+
 #include<iostream>
 using namespace std;
-    void print6(int n) {
-        for(int i=0; i<n; i++) {
-                // space
-
-        for(int j=0; j<n-i-1; j++) {
-                cout<< " ";
+void print8(int n) {
+        for(int i = 1; i<=2*n-i; i++) {
+                int stars = i;
+                if( i>n ) stars = 2*n-i;
+                for(int j=1; j<=stars; j++) {
+                        cout<<"*";
+                }
+                cout<<endl;
         }
-                // star
-        for(int j=0; j<2*i+1; j++) {
-                cout<<"*";
-        }
-                // space
-        for(int j=0; j<n-i-1; j++) {
-                    cout<<" ";
-        }
-            cout<<endl;
-        }
-    }
+}
 int main() {
-    int t;
-    cin>>t;
-    int n;
-    cin>>n;
-    print6(t);
+        int t;
+        cin>> t;
+        for(int i=0; i<t; i++) {
+                int n; 
+                cin>>n;
+                print8(n);
+        }
 }
