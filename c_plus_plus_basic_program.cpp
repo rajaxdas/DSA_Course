0d0bb4810614f5104c81c038464f3ace90b7ab1c
Raja <rajaaax7@gmail.com>
2026-09-21 23:29:01 +0530

    Upload

--- c_plus_plus_basic_program.cpp
+++ c_plus_plus_basic_program.cpp
@@ -18,21 +18,48 @@
 
 
 // factorail of a number....
-#include<iostream>
-using namespace std;
-int main() {
-    int n, i;
-    long long fact = 1;
-    cout<<"Enter a number: ";
-    cin>>n;
-    if(n < 0) {
-        cout<<"Factorial is not defined for negative numbers";
-    }
-    else {
-        for(int i=1; i<=n; i++) {
-        fact *=i;
-        }
-        cout<<"Factorial of " << n << " = " << fact;
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n, i;
+//     long long fact = 1;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     if(n < 0) {
+//         cout<<"Factorial is not defined for negative numbers";
+//     }
+//     else {
+//         for(int i=1; i<=n; i++) {
+//         fact *=i;
+//         }
+//         cout<<"Factorial of " << n << " = " << fact;
+//     }
+//     return 0;
+// }
+
+
+
+
+//find greater number among three number
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a, b, c;
+//     cout<<"Enter first number: ";
+//     cin>>a;
+//     cout<<"Enter second number: ";
+//     cin>>b;
+//     cout<<"Enter third number: ";
+//     cin>>c;
+
+
+//     if(a>=b && a>=c) {
+//         cout<<"The greater number is: "<<a;
+//     } else if(b>=a && b>=c) {
+//         cout<<"The greater number is: "<<b;
+//     } else {
+//         cout<<"The grater value is: "<<c;
+//     }
+//     return 0;
+// }
